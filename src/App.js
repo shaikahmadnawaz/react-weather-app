0ba61adf0ddf028cbe0944556637e322c53b27ff
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-03 00:14:12 +0530

    have to complete it

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid={process.env.REACT_APP_API_KEY}`;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=guntur&appid=process.env.REACT_APP_API_KEY`;
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="top"></div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
