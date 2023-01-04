486de31b3e25479f7a20d5411d2062074187dfcd
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-04 18:02:16 +0530

    search bar css done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=process.env.REACT_APP_API_KEY`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_API_KEY}`;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
@@ -14,6 +14,7 @@ function App() {
         setData(response.data);
         console.log(response.data);
       });
+      setLocation("");
     }
   };
 
