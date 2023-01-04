eac3b68ee7f656bd77f71d27fb22817a972469fa
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-01-04 17:29:59 +0530

    fetching data code written

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import axios from "axios";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=guntur&appid=process.env.REACT_APP_API_KEY`;
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=process.env.REACT_APP_API_KEY`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
 
   return (
     <div className="app">
